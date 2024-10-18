94c607cbd44c2a2bb535af94a55f469d571b899a
jameswu05 <145163478+jameswu05@users.noreply.github.com>
2024-10-18 16:57:16 -0400

    pytorch

--- James-Notes/main.py
+++ James-Notes/main.py
@@ -1,2 +1,19 @@
+import math
 import torch
-print(torch)
+
+weights = torch.randn(784, 10) / math.sqrt(784)
+weights.requires_grad_()
+bias = torch.zeros(10, requires_grad=True)
+
+def log_softmax(x):
+    return x - x.exp().sum(-1).log().unsqueeze(-1)
+
+def model(xb):
+    return log_softmax(xb @ weights + bias)
+
+bs = 64  # batch size
+
+xb = x_train[0:bs]  # a mini-batch from x
+preds = model(xb)  # predictions
+preds[0], preds.shape
+print(preds[0], preds.shape)
